dc48e65afc8e5ec8f392ea2345d69a90b11e745f
noot <elizabethjbinks@gmail.com>
2022-06-26 14:29:17 -0400

    add get prover key to ui script

--- scripts/start-ui.sh
+++ scripts/start-ui.sh
@@ -3,6 +3,14 @@
 # TODO: pull circuit_final.zkey
 ganache-cli -d &> ganache.log &
 cd ui
+
+if [ -f ./public/circuit_final.zkey ]; then
+    echo "circuit_final.zkey already exists. Skipping."
+else
+    echo 'Downloading circuit_final.zkey'
+    wget https://cf-templates-c6c45avhvbeb-ca-central-1.s3.ca-central-1.amazonaws.com/circuit_final.zkey
+fi
+
 yarn compile
 yarn deploy 
 yarn dev
